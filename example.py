e776ffc6a50e181dfacb10e0503e2c31a882ae7d
Martijn van den Ende <martijnende@gmail.com>
2025-10-30 10:40:10 +0100

    Updated comments to have more reasonable line breaks

--- example.py
+++ example.py
@@ -12,24 +12,20 @@ y = np.sin(x) + 0.05 * np.random.randn(N)
 dy_dx_true = np.cos(x)
 
 # Acceptance threshold
-# This value should be proportional to
-# the expected level of noise. Higher
-# values give more robust gradients
-# at the expense of a lower sensitivity
-# (smearing in time/space).
+# This value should be proportional to the expected level of noise.
+# Higher values give more robust gradients at the expense of a lower
+# sensitivity (smearing in time/space).
 e = 0.1
 
 # The maximum size of the search window.
 Nmax = 10
 
-# Fit a straight line (poly=0) or a
-# 2nd-order polynomial (poly=1).
+# Fit a straight line (poly=0) or a 2nd-order polynomial (poly=1).
 # Usually a straight line is good enough
 poly = 0
 
 # Estimate the noisy gradient
-# This returns a NumPy array with the
-# following rows:
+# This returns a NumPy array with the following rows:
 # [0] = estimated slope
 # [1] = window size
 # [2] = |Δy| (should be close to e)
